5b1b250c50353a70deb4d3eb86ba0cab70b5f2e7
Pradeep Sanjaya <pradeep.sanjaya@madmobile.com>
2023-06-19 10:56:15 +0530

    Initial commit

---
+++ src/main/java/com/cake/projectversion/ProjectVersionApplication.java
@@ -0,0 +1,13 @@
+package com.cake.projectversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVersionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cake/projectversion/ProjectVersionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cake.projectversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVersionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
